4565b0c442e31488346f365b3b3d565cf6370536
Nobutaka Shiraki <shiraki.devi@gmail.com>
2020-12-09 11:36:24 +0900

    プレイヤーのステータスとはしごの昇降時のアニメーションを追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,8 +16,17 @@ public class Player : MonoBehaviour
 	Animator animator;
 	// はしごに触れているかどうか
 	bool isLadder = false;
-	// はしごを昇降中かどうか
-	bool isClimb = false;
+
+	// プレイヤーの状態
+	enum PlayerStatus
+	{
+		Idle,
+		Run,
+		Climb
+	}
+
+	// プレイヤーの状態を格納
+	PlayerStatus status = PlayerStatus.Idle;
 
 	// Start is called before the first frame update
 	void Start()
@@ -29,61 +38,55 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		text.text = "isLadder: " + isLadder + "\n" + "isClimb: " + isClimb + "\n";
+		text.text = "isLadder: " + isLadder + "\n" + "status: " + status + "\n";
 
 		float horizontalKey = Input.GetAxisRaw("Horizontal");
 		float verticalKey = Input.GetAxisRaw("Vertical");
-		// アニメーション
-		if (horizontalKey == 0)
-		{
-			animator.SetBool("isRun", false);
-		}
-		else
-		{
-			animator.SetBool("isRun", true);
-		}
-
-		if (horizontalKey > 0)
-		{
-			transform.localScale = new Vector3(1, 1, 1);
-		}
-		if (horizontalKey < 0)
-		{
-			transform.localScale = new Vector3(-1, 1, 1);
-		}
 
 		if (isLadder)
 		{
 			// はしごに触れている状態で[↑]または[↓]キー押下した場合
 			if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
 			{
-				// はしご昇降中のフラグを立てる
-				isClimb = true;
+				// はしご昇降中の状態にする
+				status = PlayerStatus.Climb;
 			}
 		}
 
-		if (isClimb)
+		if (status == PlayerStatus.Climb)
 		{
+			// ハシゴに触れていて昇降中の場合
 			rb.velocity = new Vector2(horizontalKey * moveSpeed, verticalKey * moveSpeed);
 			rb.gravityScale = 0;
+			// 一方通行の床のColliderを無効にする
 			oneWay.GetComponent<BoxCollider2D>().enabled = false;
 		}
 		else
 		{
+			// 昇降中でない場合
 			rb.velocity = new Vector2(horizontalKey * moveSpeed, rb.velocity.y);
 			rb.gravityScale = 4f;
+			// 一方通行の床のColliderを有効にする
 			oneWay.GetComponent<BoxCollider2D>().enabled = true;
-		}
 
-		// はしごに触れていてかつ昇降中の場合
-		// 一方通行の床のColliderを無効にする
-		if (isLadder && isClimb)
-		{
-			oneWay.GetComponent<BoxCollider2D>().enabled = false;
-		}
-		else
-		{
-			oneWay.GetComponent<BoxCollider2D>().enabled = true;
+			if (horizontalKey == 0)
+			{
+				status = PlayerStatus.Idle;
+			}
+			else
+			{
+				status = PlayerStatus.Run;
+			}
+
+			// スプライトの反転
+			if (horizontalKey > 0)
+			{
+				transform.localScale = new Vector3(1, 1, 1);
+			}
+			if (horizontalKey < 0)
+			{
+				transform.localScale = new Vector3(-1, 1, 1);
+			}
 		}
 
 		// 移動範囲を補正
@@ -93,6 +96,28 @@ public class Player : MonoBehaviour
 			pos.y,
 			pos.z);
 		transform.position = pos;
+
+		// アニメーション
+		switch (status)
+		{
+			case PlayerStatus.Idle:
+				animator.SetTrigger("Idle");
+				break;
+			case PlayerStatus.Run:
+				animator.SetTrigger("Run");
+				break;
+			case PlayerStatus.Climb:
+				// ハシゴ昇降中
+				animator.SetTrigger("Climb");
+				animator.SetFloat("Speed", verticalKey == 0 ? 0.0f : 1.0f);
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void UpdateAnimator(PlayerStatus status, float verticalKey)
+	{
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
@@ -115,7 +140,7 @@ public class Player : MonoBehaviour
 		if (collision.gameObject.CompareTag("Ladder"))
 		{
 			isLadder = false;
-			isClimb = false;
+			status = PlayerStatus.Idle;
 		}
 	}
 }
